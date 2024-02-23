10d6ad3e34f89c9929aebdc2ddfc158bfe688791
sergiovalbuena <iosergio.valbuena@gmail.com>
2024-02-22 16:40:21 -0800

    Add feature highlights section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,13 @@ import {
 } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
 import { Slider } from "@/components/ui/slider";
+import { beep } from "@/utils/audio";
 import {
   Camera,
   FlipHorizontal,
+  MoonIcon,
   PersonStanding,
+  SunIcon,
   Video,
   Volume2,
 } from "lucide-react";
@@ -104,7 +107,7 @@ const HomePage = (props: Props) => {
             <div className="flex flex-col gap-2">
               <Separator className="my-2" />
               <Popover>
-                <PopoverTrigger>
+                <PopoverTrigger asChild>
                   <Button variant={"outline"} size={"icon"}>
                     <Volume2 />
                   </Button>
@@ -123,6 +126,10 @@ const HomePage = (props: Props) => {
                 </PopoverContent>
               </Popover>
             </div>
+          </div>
+
+          <div className="h-full flex-1 py-4 px-2 overflow-y-scroll ">
+            <RenderFeatureHighlightsSection />
           </div>
         </div>
       </div>
@@ -153,6 +160,107 @@ const HomePage = (props: Props) => {
       toast("Auto record enabled");
       //show toast to user to notify that auto record is enabled
     }
+  }
+
+  // inner components
+  function RenderFeatureHighlightsSection() {
+    return (
+      <div className="text-xs text-muted-foreground">
+        <ul className="space-y-4">
+          <li>
+            <strong>Dark Mode/Sys Theme 🌗</strong>
+            <p>Toggle between dark mode and system theme.</p>
+            <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+              <SunIcon size={14} />
+            </Button>{" "}
+            /{" "}
+            <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+              <MoonIcon size={14} />
+            </Button>
+          </li>
+          <li>
+            <strong>Horizontal Flip ↔️</strong>
+            <p>Adjust horizontal orientation.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => {
+                setMirrored((prev) => !prev);
+              }}
+            >
+              <FlipHorizontal size={14} />
+            </Button>
+          </li>
+          <Separator />
+          <li>
+            <strong>Take Pictures 📸</strong>
+            <p>Capture snapshots at any moment from the video feed.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={"outline"}
+              size={"icon"}
+              onClick={userPromptScreenshot}
+            >
+              <Camera size={14} />
+            </Button>
+          </li>
+          <li>
+            <strong>Manual Video Recording 📽️</strong>
+            <p>Manually record video clips as needed.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={isRecording ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={userPromptRecord}
+            >
+              <Video size={14} />
+            </Button>
+          </li>
+          <Separator />
+          <li>
+            <strong>Enable/Disable Auto Record 🚫</strong>
+            <p>
+              Option to enable/disable automatic video recording whenever
+              required.
+            </p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={autoRecordEnabled ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={toggleAutoRecord}
+            >
+              {autoRecordEnabled ? (
+                <Rings color="white" height={30} />
+              ) : (
+                <PersonStanding size={14} />
+              )}
+            </Button>
+          </li>
+
+          <li>
+            <strong>Volume Slider 🔊</strong>
+            <p>Adjust the volume level of the notifications.</p>
+          </li>
+          <li>
+            <strong>Camera Feed Highlighting 🎨</strong>
+            <p>
+              Highlights persons in{" "}
+              <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
+              <span style={{ color: "#00B612" }}>green</span>.
+            </p>
+          </li>
+          <Separator />
+          <li className="space-y-4">
+            <strong>Share your thoughts 💬 </strong>
+            {/* <SocialMediaLinks /> */}
+            <br />
+            <br />
+            <br />
+          </li>
+        </ul>
+      </div>
+    );
   }
 };
 
